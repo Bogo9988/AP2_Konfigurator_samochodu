5ab23562426213d24cb8cc2140ae86a3ad8dc87d
Bogo9988 <7szymo@gmail.com>
2025-03-23 16:10:01 +0100

    stworzenie klasy mainactivity dodaje wybór typu samochodu i aktualizację obrazu

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,3 +3,31 @@ package com.example.carconfigurator
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val carImageView = findViewById<ImageView>(R.id.carImageView)
+        val carTypeGroup = findViewById<RadioGroup>(R.id.carTypeGroup)
+        val cbAirConditioning = findViewById<CheckBox>(R.id.cbAirConditioning)
+        val cbLeatherSeats = findViewById<CheckBox>(R.id.cbLeatherSeats)
+        val btnShowSummary = findViewById<Button>(R.id.btnShowSummary)
+        val tvSummary = findViewById<TextView>(R.id.tvSummary)
+
+        carTypeGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rbSedan -> carImageView.setImageResource(R.drawable.sedan)
+                R.id.rbSUV -> carImageView.setImageResource(R.drawable.suv)
+                R.id.rbHatchback -> carImageView.setImageResource(R.drawable.hatchback)
+            }
+        }
+
+        btnShowSummary.setOnClickListener {
+            val selectedCarType = when (carTypeGroup.checkedRadioButtonId) {
+                R.id.rbSedan -> "Sedan"
+                R.id.rbSUV -> "SUV"
+                R.id.rbHatchback -> "Hatchback"
+                else -> ""
+            }
